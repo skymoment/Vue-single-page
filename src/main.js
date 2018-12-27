dc039336abd95c4636954949d650c421e446490e
wangzhenyu <741502286@qq.com>
2018-12-27 18:04:20 +0800

    引用 Vuetify

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+
+// 使用 Vuetify 移动端框架
+Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
